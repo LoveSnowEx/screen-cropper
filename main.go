6ffb598fdbc5a9816bb1b400fea14850a0473f65
LoveSnowEx <lsktw22@gmail.com>
2023-06-28 05:58:39 +0000

    Apply ScreenHandler to AssetServer

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 
+	"github.com/LoveSnowEx/screen-cropper/pkg/handler"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
@@ -21,7 +22,8 @@ func main() {
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
-			Assets: assets,
+			Assets:  assets,
+			Handler: handler.NewScreenHandler(app.screen),
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
